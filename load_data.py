85f29df3589553d9ff72a2c21ede5df181695c6f
Dam Hiep <hiep@kiuglobal.com>
2020-03-31 12:58:13 +0700

    fix(load data): filter files

--- load_data.py
+++ load_data.py
@@ -4,6 +4,6 @@ from os import listdir
 from os.path import isfile, join
 
 if __name__ == '__main__':
-    files = [f for f in listdir('./fixtures') if isfile(join('./fixtures', f))]
+    files = [f for f in listdir('./fixtures') if isfile(join('./fixtures', f)) and f.endswith('.json')]
     for f in files:
         os.system('python manage.py loaddata fixtures/%s' % f)
